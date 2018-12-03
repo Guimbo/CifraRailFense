d4c440a25742444eca7b4d6057e40ed87b1b1add
Gabriel Palhares <gaapalhares@gmail.com>
2018-12-03 16:40:58 -0300

    treta

--- RailFense_C/main.c
+++ RailFense_C/main.c
@@ -32,20 +32,20 @@ void encryptMsg( Mensagem* mensagem, int chave){
         row = row + inversor;
     }
 
-    printf("\n\nMensagem Cifrada: \n\n");
+//    printf("\n\nMensagem Cifrada: \n\n");
 
     for(line_matrix = 0; line_matrix < chave; ++line_matrix)
         for(col_matrix = 0; col_matrix < size_msg; ++col_matrix)
             if(railMatrix[line_matrix][col_matrix] != '\n'){
                 mensagem->cifrado[u++] = railMatrix[line_matrix][col_matrix];
             }
-    printf("\n%s\n", mensagem->cifrado);
+//    printf("\n%s\n", mensagem->cifrado);
 
 
     texto_cifra_rail_fense = fopen("texto_cifra_rail_fense.txt", "wb");
     int result = fputs(mensagem->cifrado, texto_cifra_rail_fense);
     if(result == EOF){
-        printf("Deu ruim");
+//        printf("Deu ruim");
         return 1;
     }
 
@@ -53,83 +53,6 @@ void encryptMsg( Mensagem* mensagem, int chave){
 
 }
 
-void decryptMsg(Mensagem * mensagem, int chave){
-    int size_msg = strlen(mensagem->cifrado);
-    int line_matrix;
-    int col_matrix;
-    int inversor = -1;
-    int row = 0;
-    int col = 0;
-    int m = 0;
-    int u = 0;
-    char railMatrix[chave][size_msg];
-    FILE *texto_cifra_rail_fense;
-    FILE *texto__rail_fense_decifrado;
-
-    texto_cifra_rail_fense = fopen("texto_cifra_rail_fense.txt", "rb");
-
-    if(texto_cifra_rail_fense == NULL){
-        printf("Deu ruim");
-        return 1;
-    }
-    int i, ch;;
-
-    i = 0;
-    while (i < sizeof(mensagem->cifrado)-1 && EOF !=(ch=fgetc(texto_cifra_rail_fense))){
-        mensagem->cifrado[i++] = ch;
-    }
-    fclose(texto_cifra_rail_fense);
-
-
-    for(line_matrix = 0; line_matrix < chave; ++line_matrix){
-        for(col_matrix = 0; col_matrix < size_msg; ++col_matrix){
-            railMatrix[line_matrix][col_matrix] = '\n';
-        }
-    }
-
-
-
-
-    for(line_matrix = 0; line_matrix < size_msg; ++line_matrix){
-        railMatrix[row][col++] = '*';
-
-        if(row == 0 || row == chave-1){
-            inversor= inversor * (-1);
-        }
-
-        row = row + inversor;
-    }
-
-    for(line_matrix = 0; line_matrix < chave; ++line_matrix)
-        for(col_matrix = 0; col_matrix < size_msg; ++col_matrix)
-            if(railMatrix[line_matrix][col_matrix] == '*'){
-                railMatrix[line_matrix][col_matrix] = mensagem->cifrado[m++];
-            }
-
-    row = col = 0;
-    inversor = -1;
-
-    printf("\n\nMensagem Decifrada\n\n");
-
-    for(line_matrix = 0; line_matrix < size_msg; ++line_matrix){
-        mensagem->decifrado[u++] = railMatrix[row][col++];
-        if(row == 0 || row == chave-1){
-            inversor= inversor * (-1);
-        }
-
-        row = row + inversor;
-    }
-    printf("\n%s\n", mensagem->decifrado);
-
-    texto__rail_fense_decifrado = fopen("texto__rail_fense_decifrado.txt", "wb");
-    int result = fputs( mensagem->decifrado, texto__rail_fense_decifrado);
-    if(result == EOF){
-        printf("Deu ruim");
-        return 1;
-    }
-
-    fclose(texto__rail_fense_decifrado);
-}
 
 void addMensagem(char msg1[], Mensagem* mesNew){
     int line_matrix;
